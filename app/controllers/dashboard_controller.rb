753c5617eccb74272b912aded1db9fc3be0662e0
tuxnotfound <tuxnotfound@cioga.eu>
2026-03-28 09:40:06 +0000

    Improve timeline entries management

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   before_action :require_authentication
 
   def index
+    @entries          = current_user.entries.visible.chronological
+    @hidden_entries   = current_user.entries.where(hidden: true).chronological
     @github_repos     = current_user.github_repos.order(:name)
     @last_github_sync = current_user.last_synced_at(source: :github)
   end
