9e5a0b4e932962ed04ce53ea6032e0d5e2ee4adb
tuxnotfound <tuxnotfound@cioga.eu>
2026-05-08 15:26:05 +0100

    dashboard: add share on X button for entries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+  def tinyurl_for(url)
+    Rails.cache.fetch("tinyurl_v2:#{url.hash.abs}", expires_in: 30.days) do
+      response = Faraday.get("https://tinyurl.com/api-create.php") do |req|
+        req.params[:url] = url
+        req.options.timeout = 3
+      end
+      short = response.body.strip
+      raise unless short.match?(/\Ahttps?:\/\/tinyurl\.com/)
+      short
+    end
+  rescue
+    url
+  end
+
   def streak_emoji(streak)
     case streak
     when 1    then "🐢"
